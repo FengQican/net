8be1e16ef51fc2dc5f2f5898b565a57b5ff61a32
Qican Feng <hlaite@gmail.com>
2013-12-06 11:23:59 +0800

    pushToNetTest

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Qican Feng on 13-11-28.
+//  Copyright (c) 2013年 Feng Qican. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
