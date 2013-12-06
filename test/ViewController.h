45d860f07d4b346cd1b7809c1d1179f4034af237
Qican Feng <hlaite@gmail.com>
2013-12-06 11:29:21 +0800

    addFile

---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Qican Feng on 13-11-28.
+//  Copyright (c) 2013年 Feng Qican. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by Qican Feng on 13-11-28.
+//  Copyright (c) 2013年 Feng Qican. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+
+   
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
